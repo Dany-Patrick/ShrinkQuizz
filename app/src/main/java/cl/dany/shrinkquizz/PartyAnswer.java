d959c7e359c62e4bcc09ce8c0957471ac4153b4b
Dany <davergarap@uc.cl>
2018-04-03 00:08:03 -0300

    first commit

---
+++ app/src/main/java/cl/dany/shrinkquizz/PartyAnswer.java
@@ -0,0 +1,17 @@
+package cl.dany.shrinkquizz;
+
+public class PartyAnswer {
+    public String answer;
+
+    public PartyAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
